a72a8e4fd9a7beda0a2cda54266d1e5a7bb0c6f3
AIE\guest040 <simescameron17@gmail.com>
2023-07-11 11:50:08 +0930

    added assets in, final level design complete

--- Ululatus 2/Assets/VHS/Scripts/VHSPostProcessEffect.cs
+++ Ululatus 2/Assets/VHS/Scripts/VHSPostProcessEffect.cs
@@ -14,37 +14,56 @@ public class VHSPostProcessEffect : MonoBehaviour
 	private float _xScanline;
 	private Material _material = null;
 	private VideoPlayer _player;
+	public bool EnableEffect;
 
 	void Start()
 	{
-		_material = new Material(shader);
-		_player = GetComponent<VideoPlayer>();
-		_player.isLooping = true;
-		_player.renderMode = VideoRenderMode.APIOnly;
-		_player.audioOutputMode = VideoAudioOutputMode.None;
-		_player.clip = VHSClip;
-		_player.Play();
+		Debug.Log("VHS is waiting.");
 	}
 
 	void OnRenderImage(RenderTexture source, RenderTexture destination)
 	{
-		_material.SetTexture("_VHSTex", _player.texture);
+		if (EnableEffect == true){
+            Debug.Log("VHS is enabled");
+            _material = new Material(shader);
+            _player = GetComponent<VideoPlayer>();
+            _player.isLooping = true;
+            _player.renderMode = VideoRenderMode.APIOnly;
+            _player.audioOutputMode = VideoAudioOutputMode.None;
+            _player.clip = VHSClip;
+            _player.Play();
 
-		_yScanline += Time.deltaTime * 0.01f;
-		_xScanline -= Time.deltaTime * 0.1f;
+            _material.SetTexture("_VHSTex", _player.texture);
 
-		if (_yScanline >= 1)
+			_yScanline += Time.deltaTime * 0.01f;
+			_xScanline -= Time.deltaTime * 0.1f;
+
+			if (_yScanline >= 1)
+			{
+				_yScanline = Random.value;
+			}
+			if (_xScanline <= 0 || Random.value < 0.05)
+			{
+				_xScanline = Random.value;
+			}
+			_material.SetFloat("_yScanline", _yScanline);
+			_material.SetFloat("_xScanline", _xScanline);
+			Graphics.Blit(source, destination, _material);
+        }
+		else
 		{
-			_yScanline = Random.value;
-		}
-		if (_xScanline <= 0 || Random.value < 0.05)
-		{
-			_xScanline = Random.value;
-		}
-		_material.SetFloat("_yScanline", _yScanline);
-		_material.SetFloat("_xScanline", _xScanline);
-		Graphics.Blit(source, destination, _material);
-	}
+			_material = new Material(shader);
+            _player = GetComponent<VideoPlayer>();
+            _player.isLooping = true;
+            _player.renderMode = VideoRenderMode.APIOnly;
+            _player.audioOutputMode = VideoAudioOutputMode.None;
+            _player.clip = VHSClip;
+            _player.Play();
+
+            _material.SetTexture("_VHSTex", _player.texture);
+            Graphics.Blit(null, null, _material);
+        }
+    }
 
 	protected void OnDisable()
 	{
